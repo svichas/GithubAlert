d16422a15750772c2e42194bf48fb25529dda2a3
svichas <stefanos@vichas.org>
2018-03-10 01:12:09 +0200

    Added file stracture

---
+++ githubalert.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* Author: Stefanos Vichas
+* Version: 0.1.0
+* Project: https://github.com/svichas/GithubAlert
+* license: MIT
+*/
+
+
+/**
+* Main GithubAlert class
+*/
+class GithubAlert {
+
+  public function api_serve() {
+
+
+
+  }
+
+}
